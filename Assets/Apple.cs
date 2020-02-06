d99700911a4d1ca6daa0b62e5c532da7b7d9c363
woojink3r <57843107+woojink3r@users.noreply.github.com>
2020-02-06 09:17:26 -0500

    Destroy Baskets

--- Assets/Apple.cs
+++ Assets/Apple.cs
@@ -9,6 +9,11 @@ public class Apple : MonoBehaviour
 		if (transform.position.y < bottomY)
 		{
 			Destroy(this.gameObject);
+
+			//Get a reference to the ApplePicker component of Main Camera
+			ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+			//Call the public AppleDestroyed() method of apScript
+			apScript.AppleDestroyed();
 		}
 	}
 }
--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 public class ApplePicker : MonoBehaviour
 {
 	[Header("Set in	Inspector")]
@@ -8,15 +9,37 @@ public class ApplePicker : MonoBehaviour
 	public int numBaskets = 3;
 	public float basketBottomY = -14f;
 	public float basketSpacingY = 2f;
+	public List<GameObject> basketList;
 	void Start()
 	{
+		basketList = new List<GameObject>();
 		for (int i = 0; i < numBaskets; i++)
 		{
 			GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
 			Vector3 pos = Vector3.zero;
 			pos.y = basketBottomY + (basketSpacingY * i);
 			tBasketGO.transform.position = pos;
+			basketList.Add(tBasketGO);
 		}
+	}
+
+	public void AppleDestroyed()
+	{
+		//Destroy all of the falling apples
+		GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+		foreach(GameObject tGO in tAppleArray)
+		{
+			Destroy(tGO);
+		}
+
+		//Destroy one of the baskets
+		//Get the index of the last Basket in basketList
+		int basketIndex = basketList.Count - 1;
+		//Get a reference to that Basket GameObject
+		GameObject tBasketGO = basketList[basketIndex];
+		//Remove the Basket from the list and destroy the GameObject
+		basketList.RemoveAt(basketIndex);
+		Destroy(tBasketGO);
 	}
 }
 
