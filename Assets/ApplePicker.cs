1475cac76c97fb4b295cc5f1b2627c971236b3e0
woojink3r <57843107+woojink3r@users.noreply.github.com>
2020-02-06 08:51:37 -0500

    Catch Apple

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class ApplePicker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[Header("Set in	Inspector")]
+	public GameObject basketPrefab;
+	public int numBaskets = 3;
+	public float basketBottomY = -14f;
+	public float basketSpacingY = 2f;
+	void Start()
+	{
+		for (int i = 0; i < numBaskets; i++)
+		{
+			GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
+			Vector3 pos = Vector3.zero;
+			pos.y = basketBottomY + (basketSpacingY * i);
+			tBasketGO.transform.position = pos;
+		}
+	}
 }
+
--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Basket : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	void Update()
+	{
+		// Get the current screen position of the mouse from Input
+		Vector3 mousePos2D = Input.mousePosition;                                                                                                            
+		// The	Camera's z position sets how far to	push the mouse into 3D
+		mousePos2D.z = -Camera.main.transform.position.z;                                                                       
+		// Convert the point from 2D screen space into 3D game world space
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+		// Move the	x position of this Basket to the x position	of the Mouse
+		Vector3 pos = this.transform.position;
+		pos.x = mousePos3D.x;
+		this.transform.position = pos;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void OnCollisionEnter(Collision coll)
+	{                                                                   
+		// Find out what hit this basket
+		GameObject collidedWith = coll.gameObject;                                                                             
+		if (collidedWith.tag == "Apple")
+		{                                                                                                       
+			Destroy(collidedWith);
+		}
+	}
+
 }
