c202a969ce5d10e6d0e3cdc0f3f5321b760305e3
woojink3r <57843107+woojink3r@users.noreply.github.com>
2020-02-06 08:30:37 -0500

    Stop Apple Dropping

--- Assets/Apple.cs
+++ Assets/Apple.cs
@@ -1,18 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Apple : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public static float bottomY = -20f;
+	void Update()
+	{
+		if (transform.position.y < bottomY)
+		{
+			Destroy(this.gameObject);
+		}
+	}
 }
+
