1c35b5ae5a74d6832fd916c1a1d2eb8a11fa62d6
woojink3r <57843107+woojink3r@users.noreply.github.com>
2020-02-06 08:06:06 -0500

    AppleTree movement

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -21,6 +21,10 @@ public class AppleTree : MonoBehaviour
 	void Update()
 	{
 		//Basic Movement
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+
 		//Changing Direction
 	}
 }
